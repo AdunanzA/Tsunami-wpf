a14c3417009153a2372f5700ce357bd7524f7823
gobne <tomcos61@libero.it>
2016-04-13 13:59:22 +0200

    Added PageContainer on MainWindow

--- GUI_WPF/MainWindow.xaml.cs
+++ GUI_WPF/MainWindow.xaml.cs
@@ -39,7 +39,7 @@ namespace Tsunami.Gui.Wpf
 
         private void RemovedFromTsunamiCore(object sender, SessionManager.OnTorrentRemovedEventArgs e)
         {
-
+            
         }
 
         private void AddFromTsunamiCore(object sender, SessionManager.OnTorrentAddedEventArgs e)
@@ -56,74 +56,6 @@ namespace Tsunami.Gui.Wpf
             //context.Clients.All.notifyUpdateProgress(e);
         }
 
-        private void AutoKill_Click(object sender, RoutedEventArgs e)
-        {
-            //Environment.Exit(0);
-            //var set = new Settings();
-            //set.PATH_DOWNLOAD = "ciao";
-        }
-
-        private void ReadXML_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Non toccare cio' che non comprendi!" + Environment.NewLine + "Feature in development!");
-            //    var ds = new DataSet;
-            //    ds.ReadXml(Environment.CurrentDirectory  + "/config-core.xml");
-        }
-
-        private void AddTorrent_Click(object sender, RoutedEventArgs e)
-        {
-            var df = new TextDataFormat();
-            // se c'è un link negli appunti allora lo aggiungo ai download 
-            // altrimenti chiedo di indicare un file torrent
-            if (Clipboard.GetText(df).Contains("magnet"))
-            {
-                string message = "Aggiungere questo magnet ai downloads ? ";
-                string caption = "Confirmation";
-                MessageBoxButton buttons = MessageBoxButton.YesNo;
-                MessageBoxImage icon = MessageBoxImage.Question;
-                if (MessageBox.Show(message, caption, buttons, icon) == MessageBoxResult.OK)
-                {
-                    // ok aggiungo 
-                    //TorSession.add_magnet(Clipboard.GetText(df));
-                }
-                else AddTorrent();
-            }
-            else
-            {
-                AddTorrent();
-            }
-        }
-
-        public void AddTorrent()
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-            //ofd.DefaultExt = ".torrent";
-            ofd.Filter = "Torrent|*.torrent";
-            ofd.Multiselect = true;
-            ofd.CheckFileExists = true;
-            ofd.CheckPathExists = true;
-            ofd.Title = "Select Torrent to Add";
-            ofd.ShowDialog();
-            foreach (string file in ofd.FileNames)
-            {
-                SessionManager.addTorrent(file);
-            }
-            
-            //Torrentlist = new ObservableCollection<int, string, double>();
-            //SessionManager.TorrentSession.add_torrent(atp);
-            //TorrentHandle[] th = SessionManager.TorrentSession.get_torrents();
-            //foreach (TorrentHandle t in th)
-            //{
-            //    Torrentlist.Add(t.torrent_file().name());
-            //}
-            //dataGridx.ItemsSource = Torrentlist;
-        }
-
-        private void StartATorrent_Click(object sender, RoutedEventArgs e)
-        {
-            //TorSession.get_torrents();
-            //SessionManager.TorrentSession.get_torrents();
-        }
 
         public class ObservableCollection<T1, T2, T3> : ObservableCollection<string>
         {
@@ -162,17 +94,34 @@ namespace Tsunami.Gui.Wpf
             }
             this.Resources.MergedDictionaries.Add(dict);
         }
-        //gobne end
-
-        private void btnOpenWeb_Click(object sender, RoutedEventArgs e)
-        {
-            System.Diagnostics.Process.Start("http://"+Settings.User.WebAddress+":"+Settings.User.WebPort);
-        }
 
         private void Window_Closed(object sender, EventArgs e)
         {
- 
+
         }
+
+        //page navigation methods
+        private void downloadsButton_Click(object sender, RoutedEventArgs e)
+        {
+            PageContainer.Content = new Downloads();
+        }
+
+        private void searchButton_Click(object sender, RoutedEventArgs e)
+        {
+            PageContainer.Content = new Search();
+        }
+
+        private void playerButton_Click(object sender, RoutedEventArgs e)
+        {
+            PageContainer.Content = new Player();
+        }
+
+        private void sharingButton_Click(object sender, RoutedEventArgs e)
+        {
+            PageContainer.Content = new Sharing();
+        }
+
+
     }
 
 }
