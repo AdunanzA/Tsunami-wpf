7a26cd9598bdcf22c840ac0cb30a6a7e7bd54df5
gobne <tomcos61@libero.it>
2016-05-11 15:39:41 +0200

    Added Dispose method to Search.xaml.cs

--- GUI_WPF/Search.xaml.cs
+++ GUI_WPF/Search.xaml.cs
@@ -18,11 +18,46 @@ namespace Tsunami.Gui.Wpf
     /// <summary>
     /// Logica di interazione per Search.xaml
     /// </summary>
-    public partial class Search : Page
+    public partial class Search : Page, IDisposable
     {
         public Search()
         {
             InitializeComponent();
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // Per rilevare chiamate ridondanti
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: eliminare lo stato gestito (oggetti gestiti).
+                }
+
+                // TODO: liberare risorse non gestite (oggetti non gestiti) ed eseguire sotto l'override di un finalizzatore.
+                // TODO: impostare campi di grandi dimensioni su Null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: eseguire l'override di un finalizzatore solo se Dispose(bool disposing) include il codice per liberare risorse non gestite.
+        // ~Search() {
+        //   // Non modificare questo codice. Inserire il codice di pulizia in Dispose(bool disposing) sopra.
+        //   Dispose(false);
+        // }
+
+        // Questo codice viene aggiunto per implementare in modo corretto il criterio Disposable.
+        void IDisposable.Dispose()
+        {
+            // Non modificare questo codice. Inserire il codice di pulizia in Dispose(bool disposing) sopra.
+            Dispose(true);
+            // TODO: rimuovere il commento dalla riga seguente se è stato eseguito l'override del finalizzatore.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
