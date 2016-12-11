b406f5f70939197746dc2a48113274c66d5f094f
gobne <tomcos61@libero.it>
2016-12-11 18:32:12 +0100

    Setting Test Fixes

--- Tsunami/Pages/SettingsPage.xaml.cs
+++ Tsunami/Pages/SettingsPage.xaml.cs
@@ -25,70 +25,76 @@ namespace Tsunami.Pages
             InitializeComponent();
         }
 
-        private void selectFolder_Click(object sender, RoutedEventArgs e)
+        private async void BrushButton_Click(object sender, RoutedEventArgs e)
         {
-            string path = Classes.Utils.RetrieveDirectory();
-            if (!string.IsNullOrWhiteSpace(path))
-            {
-                ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
-                tvm.Preference.PathDownload = path;
-            }
-        }
-        private void cmbColor_Loaded(object sender, RoutedEventArgs e)
-        {
-            foreach (Models.Preferences.ColorItem item in cmbColor.Items)
-            {
-                if (item.Name == Settings.User.ThemeColor)
-                {
-                    cmbColor.SelectedIndex = cmbColor.Items.IndexOf(item);
-                    break;
-                }
-            }
+            var brushDiag = new BrushDialog();
+            await MaterialDesignThemes.Wpf.DialogHost.Show(brushDiag, "RootDialog");
         }
 
-        private void cmbColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (cmbColor.SelectedItem != null)
-            {
-                ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
-                Models.Preferences.ColorItem ci = (Models.Preferences.ColorItem)cmbColor.SelectedItem;
-                tvm.Preference.ThemeColor = ci.Name;
-            }
-        }
+        //private void selectFolder_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string path = Classes.Utils.RetrieveDirectory();
+        //    if (!string.IsNullOrWhiteSpace(path))
+        //    {
+        //        ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
+        //        tvm.Preference.PathDownload = path;
+        //    }
+        //}
+        //private void cmbColor_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    foreach (Models.Preferences.ColorItem item in cmbColor.Items)
+        //    {
+        //        if (item.Name == Settings.User.ThemeColor)
+        //        {
+        //            cmbColor.SelectedIndex = cmbColor.Items.IndexOf(item);
+        //            break;
+        //        }
+        //    }
+        //}
 
-        private void cmbAccent_Loaded(object sender, RoutedEventArgs e)
-        {
-            foreach (Models.Preferences.ColorItem item in cmbAccent.Items)
-            {
-                if (item.Name == Settings.User.AccentColor)
-                {
-                    cmbAccent.SelectedIndex = cmbAccent.Items.IndexOf(item);
-                    break;
-                }
-            }
-        }
+        //private void cmbColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    if (cmbColor.SelectedItem != null)
+        //    {
+        //        ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
+        //        Models.Preferences.ColorItem ci = (Models.Preferences.ColorItem)cmbColor.SelectedItem;
+        //        tvm.Preference.ThemeColor = ci.Name;
+        //    }
+        //}
 
-        private void cmbAccent_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (cmbAccent.SelectedItem != null)
-            {
-                ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
-                Models.Preferences.ColorItem ci = (Models.Preferences.ColorItem)cmbAccent.SelectedItem;
-                tvm.Preference.AccentColor = ci.Name;
-            }
-        }
+        //private void cmbAccent_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    foreach (Models.Preferences.ColorItem item in cmbAccent.Items)
+        //    {
+        //        if (item.Name == Settings.User.AccentColor)
+        //        {
+        //            cmbAccent.SelectedIndex = cmbAccent.Items.IndexOf(item);
+        //            break;
+        //        }
+        //    }
+        //}
 
-        private void confApply_Click(object sender, RoutedEventArgs e)
-        {
-            ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
-            tvm.Preference.savePreferenceToFile();
-        }
+        //private void cmbAccent_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    if (cmbAccent.SelectedItem != null)
+        //    {
+        //        ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
+        //        Models.Preferences.ColorItem ci = (Models.Preferences.ColorItem)cmbAccent.SelectedItem;
+        //        tvm.Preference.AccentColor = ci.Name;
+        //    }
+        //}
 
-        private void confCancel_Click(object sender, RoutedEventArgs e)
-        {
-            ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
-            tvm.Preference.reloadPreferenceFromFile();
-        }
+        //private void confApply_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
+        //    tvm.Preference.savePreferenceToFile();
+        //}
+
+        //private void confCancel_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
+        //    tvm.Preference.reloadPreferenceFromFile();
+        //}
 
     }
 }
