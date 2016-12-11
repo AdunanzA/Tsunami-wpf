1a5ea3025fca5f9984ecd9a2da187855d0f30575
gobne <tomcos61@libero.it>
2016-12-11 17:07:44 +0100

    New settings test

---
+++ Tsunami/Pages/BrushDialog.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Tsunami.Pages
+{
+    /// <summary>
+    /// Interaction logic for BrushDialog.xaml
+    /// </summary>
+    public partial class BrushDialog : UserControl
+    {
+        public BrushDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbColor_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (Models.Preferences.ColorItem item in cmbColor.Items)
+            {
+                if (item.Name == Settings.User.ThemeColor)
+                {
+                    cmbColor.SelectedIndex = cmbColor.Items.IndexOf(item);
+                    break;
+                }
+            }
+        }
+
+        private void cmbColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbColor.SelectedItem != null)
+            {
+                ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
+                Models.Preferences.ColorItem ci = (Models.Preferences.ColorItem)cmbColor.SelectedItem;
+                tvm.Preference.ThemeColor = ci.Name;
+            }
+        }
+
+        private void cmbAccent_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (Models.Preferences.ColorItem item in cmbAccent.Items)
+            {
+                if (item.Name == Settings.User.AccentColor)
+                {
+                    cmbAccent.SelectedIndex = cmbAccent.Items.IndexOf(item);
+                    break;
+                }
+            }
+        }
+
+        private void cmbAccent_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbAccent.SelectedItem != null)
+            {
+                ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
+                Models.Preferences.ColorItem ci = (Models.Preferences.ColorItem)cmbAccent.SelectedItem;
+                tvm.Preference.AccentColor = ci.Name;
+            }
+        }
+
+        private void confApply_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
+            tvm.Preference.savePreferenceToFile();
+        }
+
+        private void confCancel_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
+            tvm.Preference.reloadPreferenceFromFile();
+        }
+    }
+}
