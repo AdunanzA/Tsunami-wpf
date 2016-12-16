77af21b72560d39847543a729c54aee086f042cd
gobne <tomcos61@libero.it>
2016-12-16 15:18:44 +0100

    Moved Dialogs files to new Directory

---
+++ Tsunami/Pages/Dialogs/WebSettingsDialog.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tsunami.Pages.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for WebSettingsDialog.xaml
+    /// </summary>
+    public partial class WebSettingsDialog : UserControl
+    {
+        public WebSettingsDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void confApply_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
+            tvm.Preference.savePreferenceToFile();
+        }
+
+        private void confCancel_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.TsunamiViewModel tvm = (ViewModel.TsunamiViewModel)FindResource("TsunamiVM");
+            tvm.Preference.reloadPreferenceFromFile();
+        }
+    }
+}
